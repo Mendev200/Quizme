75dbc1d125c0b0b39beecdbce33fa2c054f6c5a1
Mendev200 <irca.mendev@gmail.com>
2024-03-21 15:59:35 +0100

    supression des doublons et ajout de commentaire

--- public/js/lastQuestionRedirection.js
+++ public/js/lastQuestionRedirection.js
@@ -1,10 +1,14 @@
+// Cette fonction vérifie si la question actuelle est la dernière question. Si c'est le cas, elle met à jour le lien de redirection vers "scoreScreen.html".
 function lastQuestion() {
+    // Récupération de l'ID de la question depuis le stockage de session et conversion en nombre.
     let idQuestion = Number(sessionStorage.idQuestion);
+    
+    // Vérification si l'ID de la question est égal à 10 (dernière question).
     if (idQuestion == 10) {
+        // Mise à jour du lien de redirection vers "scoreScreen.html".
         document.querySelector('a').setAttribute("href", "scoreScreen.html");
     }
 }
 
+// Appel de la fonction pour vérifier si c'est la dernière question.
 lastQuestion();
-
-
--- public/js/randomLetterColors.js
+++ public/js/randomLetterColors.js
@@ -1,24 +1,46 @@
+// Cette fonction ajoute des couleurs à chaque lettre du texte à l'intérieur des balises ayant la classe "colors".
 function addColors() {
-    const colors = ["#118C8D", "#FE4DCE", "#4591F8", "#57C124", "#FB4444",];
+    // Tableau de couleurs à utiliser pour colorer le texte.
+    const colors = ["#118C8D", "#FE4DCE", "#4591F8", "#57C124", "#FB4444"];
+    
+    // Initialisation du compteur pour parcourir le tableau de couleurs.
     let compteur = 0;
+
+    // Sélection de toutes les balises avec la classe "colors".
     let balises = document.querySelectorAll(".colors");
+
+    // Parcours de chaque balise trouvée.
     for (let balise of balises) {
+        // Récupération du texte à l'intérieur de la balise.
         let texte = balise.textContent;
+        
+        // Effacement du contenu de la balise.
         balise.textContent = "";
+
+        // Parcours de chaque lettre du texte.
         for (let lettre of texte.split("")) {
+            // Vérification si le compteur dépasse la longueur du tableau de couleurs.
             if (compteur >= colors.length) {
-                compteur = 0;
+                compteur = 0; // Réinitialisation du compteur.
             }
+
+            // Création d'un élément span pour chaque lettre.
             let element = document.createElement("span");
+            
+            // Attribution de la couleur correspondante à la lettre.
             element.style.color = colors[compteur];
+            
+            // Ajout de la lettre colorée à l'élément span.
             element.textContent = lettre;
+            
+            // Ajout de l'élément span à la balise.
             balise.appendChild(element);
+            
+            // Incrémentation du compteur pour passer à la couleur suivante.
             compteur++;
         }
     }
 }
 
+// Appel de la fonction pour ajouter des couleurs.
 addColors();
-
-
-
